9142c7c4c0494d62270bc65a1163c4a4623e1786
Ooreoluwa Ibitowa <ooreoluwaibitowa@gmail.com>
2025-10-21 11:09:31 +0100

    Initial commit: Spring Boot starter auto-generated by Agentic AI

---
+++ src/test/java/tech/justjava/InitializrtestApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.justjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializrtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
